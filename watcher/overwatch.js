2b33a6becc80108eaebf1bd19b9064d23744037d
MoeBrowne <oliver.browne@uplyme.com>
2016-03-13 20:19:37 +0000

    Added crude regex matching for PHP errors

--- watcher/overwatch.js
+++ watcher/overwatch.js
@@ -11,33 +11,50 @@ var tail = new Tail("../syslog");
 
 tail.on("line", function(data) {
 
-    //console.log(data);
-
-    var regex = /.+ ([^ ]+) apache2: \[(.+)] \[pid ([0-9]+)] \[client (.+)] \[(.+)] ([0-9]+) ([0-9]+) "([^"]+)" "([^"]+)" "([^"]+)"/g;
-
-    var dataArray = regex.exec(data);
-
-    if (dataArray === null) {
-        return false;
-    }
-
-	var serverData = {
-		server: {
-			name: dataArray[1],
-			service: {
-				name: 'apache2',
-				section: {
-					name: dataArray[2],
-					action: {
-						name: dataArray[5]
+	var serverData = {};
+    var regexApacheAccess = /.+ ([^ ]+) apache2: \[(.+)] \[pid ([0-9]+)] \[client (.+)] \[(.+)] ([0-9]+) ([0-9]+) "([^"]+)" "([^"]+)" "([^"]+)"/g;
+    var apacheAccessArray = regexApacheAccess.exec(data);
+    if (apacheAccessArray !== null) {
+		serverData = {
+			server: {
+				name: apacheAccessArray[1],
+				service: {
+					name: 'apache2',
+					section: {
+						name: apacheAccessArray[2],
+						action: {
+							name: apacheAccessArray[5]
+						}
 					}
 				}
 			}
-		}
-	};
+		};
+		console.log(serverData);
+		io.emit('data', serverData);
+		return;
+    }
 
-    console.log(serverData);
 
-    io.emit('data', serverData);
+    var regexApacheError = /.+ ([^ ]+) apache2: \[(.+)] \[error] \[pid ([0-9]+)] \[client (.+)] (.+)/g;
+    var apacheErrorArray = regexApacheError.exec(data);
+    if (apacheErrorArray !== null) {
+		serverData = {
+			server: {
+				name: apacheErrorArray[1],
+				service: {
+					name: 'php',
+					section: {
+						name: apacheErrorArray[2],
+						action: {
+							name: 'error'
+						}
+					}
+				}
+			}
+		};
+		console.log(serverData);
+		io.emit('data', serverData);
+		return;
+    }
 
 });
